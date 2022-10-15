b6411438fe1f9e7e4e278ffb1436dc6efc78887c
Aya <ayamarmash74@gmail.com>
2022-10-15 05:32:07 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) return 'This one is on me!';
+
+  if (feet <= 2000) return 'That will be twenty bucks.';
+
+  if (2000 < feet && feet <= 2500) return 'I will gladly take your thirty bucks.'; 
+
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  if(City === 'NYC') return "Ok, sounds good."
+
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  if(Tip === "generous") return 'Thank you so much.';
+
+  if(Tip === "not as generous") return 'Thank you.';
+
+  return "Bye.";
 }
